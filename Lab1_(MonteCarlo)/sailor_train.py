4bc0069ba2c2a5cf8087cac54bc18ba705fc91fb
Jakub Sachajko <kuba.sachajko@gmail.com>
2023-10-15 23:21:19 +0200

    Working Dla Wartości

--- Lab1_(MonteCarlo)/sailor_train.py
+++ Lab1_(MonteCarlo)/sailor_train.py
@@ -5,15 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sailor_funct as sf
 
-number_of_episodes = 1000                   # number of training epizodes (multi-stage processes) 
+number_of_episodes = 700000                   # number of training epizodes (multi-stage processes) 
 number_of_iterations = 100
-gamma = 0.9                                 # discount factor
+gamma = 0.90                                 # discount factor
 
 
-file_name = 'map_small.txt'
-#file_name = 'map_easy.txt'
-#file_name = 'map_big.txt'
-#file_name = 'map_spiral.txt'
+# file_name = 'map_small.txt'
+# file_name = 'map_easy.txt'
+# file_name = 'map_big.txt'
+# file_name = 'map_spiral.txt'
 
 reward_map = sf.load_data(file_name)
 num_of_rows, num_of_columns = reward_map.shape
@@ -67,7 +67,7 @@ def sailor_test(reward_map, Q, num_of_episodes):
 
 ### MONTE CARLO ITERACJA STRATEGII
 
-def monte_carlo_iteracja_strategii(reward_map, Q, num_of_episodes, num_of_iterations):
+def monte_carlo_iteracja_strategii(reward_map, Q, num_of_episodes, num_of_iterations, gammma):
     num_of_rows, num_of_columns = reward_map.shape
     num_of_steps_max = int(5 * (num_of_rows + num_of_columns))
 
@@ -128,62 +128,65 @@ def monte_carlo_iteracja_strategii(reward_map, Q, num_of_episodes, num_of_iterat
 
 ### MONTE CARLO ITERACJA WARTOŚCI
 
-def monte_carlo_iteracja_wartości(reward_map, Q, iterations, alpha):
+def monte_carlo_iteracja_wartości(reward_map, Q, iterations,epochs, gammma):
     num_of_rows, num_of_columns = reward_map.shape
     num_of_steps_max = int(5*(num_of_rows + num_of_columns))    # maximum number of steps in an episode
+    array_1 = np.arange(0, num_of_rows, 1) 
+    array_2 = np.arange(0, num_of_columns, 1) 
+    array_3 = np.arange(1, 5, 1) 
+    state_action_pairs = np.array(np.meshgrid(array_1, array_2, array_3)).T.reshape(-1, 3)
 
-    for episode in range(iterations):
-        episodeCollection = []
-        state = np.zeros([2],dtype=int)                            # initial state here [1 1] but rather random due to exploration
-        state[0] = np.random.randint(0,num_of_rows)
-        the_end = False
-        nr_pos = 0
-        # alpha_n = alpha/(episode+1)  # Adjusting alpha based on episode number
-        sum_of_rewards = 0
-        while the_end == False:
-            nr_pos = nr_pos + 1                            # move number
-            alpha_n = alpha/(nr_pos)
-            # Action choosing (1 - right, 2 - up, 3 - left, 4 - bottom): 
-            action = 1 + np.argmax(Q[state[0],state[1], :])
-            state_next, reward  = sf.environment(state, action, reward_map)
-            # episode.append((state, action, state_next, reward))
-            # Update Q-values
-            sum_of_rewards += np.power(alpha_n,nr_pos-1)*reward
+    for epoch in range(epochs):
+        alpha_counter = 0
+        for x,y,a in state_action_pairs:
+            alpha_counter += 1
+            state = np.zeros([2],dtype=int)                            # initial state here [1 1] but rather random due to exploration
+            state[0] = x
+            state[1] = y
+            the_end = False
+            nr_pos = 0
+            alpha_n = 1/(alpha_counter)  # Adjusting alpha based on episode number
+            sum_of_rewards = 0
+            while the_end == False:
+                nr_pos = nr_pos + 1                            # move number
+                # Action choosing (1 - right, 2 - up, 3 - left, 4 - bottom):
+                if nr_pos == 1:
+                    action = a
+                else: 
+                    action = 1 + np.argmax(Q[state[0],state[1], :])
+                state_next, reward  = sf.environment(state, action, reward_map)
 
-
-            Q[state[0], state[1],action-1] = (1 - alpha_n) * Q[state[0], state[1], action-1] + alpha_n * sum_of_rewards
-
-            print(action)
-            print(sum_of_rewards)
-            # print(Q)
-            # (reward + np.sum([np.power(gamma, i) * reward_map[state[0], state[1]] for i in range(nr_pos, num_of_steps_max)]))
+                # Update Q-values
+                sum_of_rewards += np.power(gamma,nr_pos-1)*reward                
+                state = state_next       # going to the next state
             
-            state = state_next       # going to the next state
-        
-            # end of episode if maximum number of steps is reached or last column is reached
-            if (nr_pos == num_of_steps_max) or (state[1] >= num_of_columns-1):
-                the_end = True
+                # end of episode if maximum number of steps is reached or last column is reached
+                if (nr_pos == num_of_steps_max) or (state[1] >= num_of_columns-1):
+                    the_end = True
 
-            print(Q)
-            for s in range(num_of_rows):
-                for t in range(num_of_columns):
-                    for a in range(4):
-                        Q[s,t, a] = (1 - alpha_n) * Q[s,t, a] + alpha_n * (reward_map[s, t] + gamma * np.max(Q[s,t, a]))
-                        print(Q)
+            Q[x,y,a-1] = (1 - alpha_n) * Q[x,y,a-1] + (alpha_n * sum_of_rewards)
+                                # print(Q)
 
-        # TESTOWANIE WYNIKÓW     
-        
-        if (episode % 100 == 0):
-            print(f"episode {episode}")
-            sf.draw(reward_map,Q)
-            print(Q)
-            print("---")
+        # if alpha_counter%5 == 0:
+        #     print(Q)
+        #     sf.draw(reward_map,Q)
+
+
+            # TESTOWANIE WYNIKÓW     
+            
+            # if (episode % 100 == 0):
+            #     print(f"episode {episode}")
+            #     sf.draw(reward_map,Q)
+            #     print(Q)
+            #     print("---")
+    return Q
 
 # QQ = monte_carlo_iteracja_strategii(reward_map=reward_map,Q=Q,num_of_episodes=1000, num_of_iterations=100)
-monte_carlo_iteracja_wartości(reward_map, Q, number_of_episodes, gamma)
+Q = monte_carlo_iteracja_wartości(reward_map, Q, number_of_episodes,number_of_iterations, gamma)
 # Q = monte_carlo_iteracja_strategii(reward_map, Q, number_of_episodes, 5)
 print("----")
 print(Q)
+sf.sailor_test(reward_map,Q,1000)
 sf.draw(reward_map,Q)
 # sf.draw(reward_map,Q)
 # # sf.draw(reward_map,Q)
